23de9500e021982bf141bb82074f5d25df655e99
Minn Maw Oo <minmawoo.ucsm@gmail.com>
2020-10-11 15:43:10 +0630

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useRef, useCallback} from 'react';
+import useBookSearch from './useBookSearch'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const {loading, error, books, hasMore} = useBookSearch(query, pageNumber)
+
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node =>{
+    if(loading) return
+    if(observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries =>{
+      if(entries[0].isIntersection && hasMore){
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if(node) observer.current.observe(node)
+  }, [loading, hasMore])
+
+  function handleSearch(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  return(
+    <>
+    <input type="text" value={query} onChange={handleSearch}></input>
+    {books.map((book, index) =>{
+      if(books.length === index + 1){
+        return <div ref={lastBookElementRef} key={book}>{book}</div>
+      }else {
+        return <div key={book}>{book}</div>
+      }
+    })}
+    <div>{loading && 'Loading ...'}</div>
+    <div>{error && 'Error ...'}</div>
+    </>
+  )
 }
-
-export default App;
